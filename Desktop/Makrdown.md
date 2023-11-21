a08a51676862f1c96e1c9a39814419cb61015d0e
gtakkou <148326961+gtakkou@users.noreply.github.com>
2023-11-21 14:29:15 +0000

    Delete Desktop directory

--- Desktop/Makrdown.md
+++
@@ -1,6 +0,0 @@
-# Fat
-## Grace
-My name is Grace and I am fat
-*Hello*
-**sexxy**
-
--- Desktop/boto3_tmp.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-s3 = boto3.client('s3')
-# s3.create_bucket(Bucket='my-boto3-bucket-takkou', CreateBucketConfiguration={'LocationConstraint': 'eu-west-1'})
-# print("Bucket created")
-s3.upload_file("C:/Users/George Takkou/Pictures/Screenshots/Screenshot 2023-11-06 214717.png", 'my-boto3-bucket-takkou', 'takkos/newfile.png')
-
